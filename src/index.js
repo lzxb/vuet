d7c1a29ad3b0f93e1e3c8139cf7e987bd2b27c6c
lzxb <1340641314@qq.com>
2017-05-22 14:34:28 +0800

    update:vertion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Vuet from './vuet'
 import install from './install'
 export { default as mapState } from './mapState'
 export { default as mapMixins } from './mapMixins'
-
 Vuet.install = install
 
 export default Vuet
