f83a4c4ed94c7456935b300b8cb379a539fc21bb
lzxb <1340641314@qq.com>
2017-08-09 11:12:30 +0800

    fix:util test

--- test/unit/util.test.js
+++ test/unit/util.test.js
@@ -2,6 +2,7 @@ import test from 'ava'
 import util from '../../src/util'
 
 test('getArgMerge', t => {
+  t.deepEqual(util.getArgMerge('name'), { name: 'name' })
   t.deepEqual(util.getArgMerge('name', 'vuet'), { name: 'vuet' })
   t.deepEqual(util.getArgMerge({ name: 'vuet' }), { name: 'vuet' })
   t.deepEqual(util.getArgMerge(), {})
@@ -14,5 +15,30 @@ test('is object', t => {
 })
 
 test('is promise', t => {
-  t.true(util.isPromise(Promise.resolve()))
+  t.false(util.isPromise(null))
+  t.false(util.isPromise(true))
+  t.false(util.isPromise(false))
+  t.false(util.isPromise(0))
+  t.false(util.isPromise([]))
+
+  // object
+  t.false(util.isPromise({}))
+  t.false(util.isPromise({
+    then: {}
+  }))
+  t.true(util.isPromise({
+    then () {}
+  }))
+
+  // function
+  t.false(util.isPromise(function () {}))
+  const myPromise = function () {}
+  myPromise.then = function () {}
+  t.true(util.isPromise(myPromise))
+  myPromise.then = []
+  t.false(util.isPromise(myPromise))
+
+  // promise
+  t.true(util.isPromise(new Promise(function () {})))
+  t.true(util.isPromise(Promise.resolve({})))
 })
